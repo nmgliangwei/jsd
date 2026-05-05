d7254d930702caaea6c60ff33c25d0e81b5b8e4f
梁伟 <nmgliangwei@gmail.com>
2026-05-05 11:21:32 +0800

    Update whitelist to include 'nmgliangwei/*'

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ export const CONFIG = {
         blacklist: [
         ],
         // 白名单模式时使用 (支持通配符，如 '@my-org/*' 允许该作用域下所有包)
-        whitelist: [
+        whitelist: [nmgliangwei/*
         ]
     },
 
